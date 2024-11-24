76ee4182e9ce94a56ce3644e24ca47d2380434fd
HAIDER MUKHTAR <76527973+HaiderMukhtar1@users.noreply.github.com>
2024-11-25 02:39:39 +0500

    New Feature Created and Implemented
    New Feature Created and Implemented

--- Assets/1- Scripts/MainMenuUI.cs
+++ Assets/1- Scripts/MainMenuUI.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using NiobiumStudios;
 
 
 
@@ -18,7 +19,32 @@ public class MainMenuUI : MonoBehaviour
 
         Time.timeScale = 1;
     }
+    void OnEnable()
+    {
+        DailyRewards.instance.onClaimPrize += OnClaimPrizeDailyRewards;
+    }
 
+    void OnDisable()
+    {
+        DailyRewards.instance.onClaimPrize -= OnClaimPrizeDailyRewards;
+    }
+
+    // this is your integration function. Can be on Start or simply a function to be called
+    public void OnClaimPrizeDailyRewards(int day)
+    {
+        //This returns a Reward object
+        Reward myReward = DailyRewards.instance.GetReward(day);
+
+        // And you can access any property
+        print(myReward.unit);   // This is your reward Unit name
+        print(myReward.reward); // This is your reward count
+
+        var rewardsCount = PlayerPrefs.GetInt("MY_REWARD_KEY", 0);
+        rewardsCount += myReward.reward;
+
+        PlayerPrefs.SetInt("MY_REWARD_KEY", rewardsCount);
+        PlayerPrefs.Save();
+    }
     public void StartGame()
     {
         Invoke(nameof(SceneChange), 4f);    }
---
+++ Assets/ShopManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+
+    public GameObject[] ShopItemButtons;//Start from 1st bow as zero
+    public int[] Prices;
+    [SerializeField]int TotalShopItems = 10;
+
+    private void Awake()
+    {
+
+        for (int i = 0; i < TotalShopItems; i++)
+        {
+            if(PlayerPrefs.GetInt("ItemUnlocked" + i) == 1)
+            {
+                ShopItemButtons[i].transform.GetChild(1).gameObject.SetActive(false);
+                ShopItemButtons[i].transform.GetChild(3).gameObject.SetActive(false);
+            }
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void BuyItem(int ShopItemNumber)//Start from 1st bow as zero
+    {
+        if (Prices[ShopItemNumber] >= PlayerPrefs.GetInt("CurrentGold"))
+        {
+            PlayerPrefs.SetInt("CurrentGold", PlayerPrefs.GetInt("CurrentGold") - Prices[ShopItemNumber]);
+            ShopItemButtons[ShopItemNumber].transform.GetChild(1).gameObject.SetActive(false);
+            ShopItemButtons[ShopItemNumber].transform.GetChild(2).gameObject.SetActive(true);
+            ShopItemButtons[ShopItemNumber].transform.GetChild(3).gameObject.SetActive(false);
+            PlayerPrefs.SetInt("ItemUnlocked" + ShopItemNumber, 1);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void SelectItem(int ShopItemNumber)
+    {
+        for (int i = 0; i < TotalShopItems; i++)
+        {
+            if (PlayerPrefs.GetInt("ItemUnlocked" + i) == 1)
+            {
+                for (int s = 0; s < ShopItemButtons.Length; s++)
+                {
+                    ShopItemButtons[s].transform.GetChild(2).gameObject.SetActive(false);
+                }
+                ShopItemButtons[i].transform.GetChild(1).gameObject.SetActive(false);
+                ShopItemButtons[i].transform.GetChild(2).gameObject.SetActive(true);
+                ShopItemButtons[i].transform.GetChild(3).gameObject.SetActive(false);
+                PlayerPrefs.SetInt("SelectedItem", ShopItemNumber);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
